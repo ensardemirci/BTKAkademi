0360d2f9f8d17ac5fa0b05a1962fab61b6035889
Ensar Demirci <59653298+ensardemirci@users.noreply.github.com>
2020-07-02 19:15:38 +0300

    Django

--- 26 - Django/catalog/catalog/settings.py
+++ 26 - Django/catalog/catalog/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'pages.apps.PagesConfig',
+    'pages',
+    'movies',
+
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'catalog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- 26 - Django/catalog/pages/urls.py
+++ 26 - Django/catalog/pages/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 # http://127.0.0.1:8000/(Bu Kısım path ilk kısımla aynı. Boş ise anasayfa)
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
 
 ]
